90d24bb308925679875b936506c54d30298092f4
JavaG23 <geof.hoffman@gmail.com>
2023-04-09 16:40:01 -0400

    homepage update

--- components/layout.js
+++ components/layout.js
@@ -6,9 +6,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
-
+const name = 'geofhoffman.com'
+export const siteTitle = 'geofhoffman.com'
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
